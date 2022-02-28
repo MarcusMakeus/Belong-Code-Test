8f33a9de15162fd90cdc7f1dd397e91f63c2bc6d
Marcus <marcus@makeusdesign.com>
2022-03-01 09:42:22 +1100

    [Initial Commit] Setup of Project

---
+++ marcus-belong-test/src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import CellPage from "./pages/CellPage";
+
+function App() {
+  return (
+    <CellPage />
+  );
+}
+
+export default App;
---
+++ marcus-belong-test/src/pages/CellPage/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
+
+const CellPage = () => {
+    return (
+        <Container maxWidth="sm" className={'h-screen p-4'}>
+            <Box className={'bg-green-200 h-full'}/>
+        </Container>
+    )
+}
+
+export default CellPage
